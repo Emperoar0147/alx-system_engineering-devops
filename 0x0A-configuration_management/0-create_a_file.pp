db06f7dab522a220097288c0f1b16c33819c35d7
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-03 20:34:10 -0700

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Puppet manifest to create a file in /tmp
 
 file { '/tmp/school':
-  ensure   => 'file',
+  ensure   => present,
   mode     => '0744',
   owner    => 'www-data',
   group    => 'www-data',
-  content  => 'I love Puppet\n',
+  content  => 'I love Puppet',
 }

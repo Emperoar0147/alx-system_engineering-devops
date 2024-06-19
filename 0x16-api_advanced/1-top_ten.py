98c1910da60d2380812a91f0569c3434b209d8b3
“Sylvanus <sylvanusuzor@gmail.com>
2024-06-19 06:31:19 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
+"""
+1-top-ten
+"""
+
 import requests
 
-def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'MyRedditAPI/0.1'}
-    params = {'limit': 10}
 
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+def top_ten(subreddit):
+    """ Print the top ten hot posts """
+    if subreddit:
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+        headers = {'User-Agent': 'Chrome'}
+        response = requests.get(url, headers=headers,  allow_redirects=False)
         if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
+            posts = response.json().get('data').get('children')
             for post in posts:
-                print(post['data']['title'])
-        else:
-            print("None")
-    except Exception:
-        print("None")
+                print(post.get('data').get('title'))
+        print('None')

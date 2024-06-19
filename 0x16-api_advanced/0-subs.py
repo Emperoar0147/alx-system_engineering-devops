60daefac2551d757725f1e3e77bfcdd24be6b301
“Sylvanus <sylvanusuzor@gmail.com>
2024-06-19 07:11:23 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
-for 0-subs
+0-subs
 """
-
 from requests import get
-
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    a function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):

3293ba254f456015c222d42ec630f27a7475c6db
“Sylvanus <sylvanusuzor@gmail.com>
2024-06-20 07:35:52 -0700

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,6 @@
 # This Puppet manifest fixes file permissions for the WordPress directory
 
-exec { 'fix-wordpress-permissions':
-  command => 'chown -R www-data:www-data /var/www/html/wordpress && chmod -R 755 /var/www/html/wordpress',
-  path    => ['/bin', '/usr/bin'],
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
-  require => Exec['fix-wordpress-permissions'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }

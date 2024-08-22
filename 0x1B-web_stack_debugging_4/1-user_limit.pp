5127994739df72f187da10af9264078f42b1c05e
“Sylvanus <sylvanusuzor@gmail.com>
2024-08-22 00:35:25 -0700

    1. User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,5 @@
-# This Puppet manifest increases the file descriptor limit for the holberton user
+# This Puppet manifest increases the file descriptor limit for the holberton user.
 
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'echo "holberton soft nofile 1024\nholberton hard nofile 2048" >> /etc/security/limits.conf',
-  unless  => 'grep -q "holberton" /etc/security/limits.conf',
-}
-
-# Reload the PAM limits to apply changes
-exec { 'reload-pam-limits':
-  command => '/sbin/sysctl -p',
-  refreshonly => true,
-  subscribe   => Exec['change-os-configuration-for-holberton-user'],
+  command => 'echo "holberton soft nofile 4096" >> /etc/security/limits.conf && echo "holberton hard nofile 4096" >> /etc/security/limits.conf',
 }

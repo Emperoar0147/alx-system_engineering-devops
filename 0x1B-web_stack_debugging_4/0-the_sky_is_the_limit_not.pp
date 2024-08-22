010f64c83b53008a038c7bba0d4d1c1543400608
“Sylvanus <sylvanusuzor@gmail.com>
2024-08-22 00:25:09 -0700

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,5 @@
-# This script increases the amount of traffic an Nginx server can handle.
+# This Puppet manifest optimizes Nginx configuration to handle high traffic efficiently
 
-# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-}  ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  command => 'sed -i "s/worker_connections 768/worker_connections 1024/" /etc/nginx/nginx.conf && service nginx restart',
 }
